3975ade1fa49514532b7217c3143b6e008ffc554
DeclanMcGauley <73233273+DeclanMcGauley1@users.noreply.github.com>
2020-12-16 16:57:15 +0000

    imported the image assets for the game

--- spaceinvader.py
+++ spaceinvader.py
@@ -2,3 +2,15 @@ import pygame
 import os
 import time
 import random
+
+#imports for the ship images
+RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
+GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
+BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
+YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
+
+#imports for the lazer images
+RED_LAZER = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
+GREEN_LAZER = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
+BLUE_LAZER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
+RED_LAZER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
