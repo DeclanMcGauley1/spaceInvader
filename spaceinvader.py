68252ffea669cf8a2ace961ac238f157002f8fb3
DeclanMcGauley <73233273+DeclanMcGauley1@users.noreply.github.com>
2020-12-16 17:09:32 +0000

    created a main function and added an event listener closing the window

--- spaceinvader.py
+++ spaceinvader.py
@@ -22,3 +22,16 @@ RED_LAZER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
 
 #imports for the background image
 BACKGROUND = pygame.image.load(os.path.join("assets", "background-black.png"))
+
+def main():
+    run = True
+    FPS = 60
+    clock = pygame.time.Clock()
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+main()
